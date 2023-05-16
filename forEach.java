3dc17977a103d5dea0e8c63ef8aa7f5cef91825b
Simone132 <simonerizzo600@gmail.com>
2023-05-16 09:30:59 +0200

    esercizio casa con errori 1

---
+++ forEach.java
@@ -0,0 +1,19 @@
+
+
+public class forEach 
+{
+    public static void main(String[] args )
+    
+    {
+    
+    String[] numbers= {c,a,b,d};
+    for (int num:)
+    
+    {
+        String dim = numbers.length;
+    
+    
+    System.out.println("numero"+ num + +dim+  "elementi" );
+}
+}
+}
